815c503c45708f2361c24d28e7cd6331aca6eb70
zhangyi <zhangyi5628@126.com>
2018-02-11 19:06:10 +0800

    add Card

---
+++ components/Card/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+import { CardProps } from './propsType'
+
+export default class Card extends React.Component<CardProps, any> {
+
+}
---
+++ components/Card/propsType.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export interface CardProps {
+    full?: boolean;
+}
+
+export interface CardHeaderProps {
+    title?: any;
+    extra?: any;
+    style?: any;
+}
+
+export interface CardFooterProps {
+    content?: any;
+}
