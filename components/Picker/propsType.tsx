baf2f470d84c4c594aa7f5d2edec7b1c4dd56266
beilunyang <786220806@qq.com>
2018-02-28 16:50:39 +0800

    picker支持popup

--- components/Picker/propsType.tsx
+++ components/Picker/propsType.tsx
@@ -13,4 +13,5 @@ export interface IPickerProps {
     title?: string;
     children?: React.ReactNode;
     headerStyle?: {};
+    visible?: boolean;
 };
--- expo-samples/demos/picker/index.js
+++ expo-samples/demos/picker/index.js
@@ -7,28 +7,63 @@ import {
     Text,
     Picker,
 } from 'react-native';
-import { Picker as YHPicker } from '../../../components/index';
+import {
+    Button,
+    WhiteSpace,
+    Picker as YHPicker,
+} from '../../../components/index';
 
 export default class PickerDemo extends Component {
     state = {
         value: '',
+        visible: false,
+        visible2: false,
     };
+
+    handleDismiss = () => {
+        this.setState({
+            visible: false,
+            visible2: false,
+        });
+    }
 
     handleOk = (selectedValue) => {
         this.setState({
             value: selectedValue,
         });
+        this.handleDismiss();
     };
+
+    handlePicker = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+
+    handlePicker2 = () => {
+        this.setState({
+            visible2: true,
+        });
+    }
 
     render() {
         return (
             <View>
                 <Text style={{ fontSize: 24, color: '#000', textAlign: 'center', height: 40 }}>{this.state.value}</Text>
+                <Button onClick={this.handlePicker}>触发Picker 1</Button>
+                <WhiteSpace/>
+                <Button onClick={this.handlePicker2}>触发Picker 2</Button>
                 <YHPicker
+                    visible={this.state.visible}
                     data={['java', 'js', 'c++', 'c#', 'python', 'lisp', 'php', 'go']}
                     onOk={this.handleOk}
+                    onDismiss={this.handleDismiss}
                 />
-                <YHPicker>
+                <YHPicker
+                    visible={this.state.visible2}
+                    onOk={this.handleOk}
+                    onDismiss={this.handleDismiss}
+                >
                     <Picker.Item value="gba" label="gba" />
                     <Picker.Item value="ps4" label="ps4" />
                     <Picker.Item value="ns" label="ns" />
