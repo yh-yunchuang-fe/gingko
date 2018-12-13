66f08912fddfe37e8790d08733c3f57f36821e0d
yh_di <wd_0315@163.com>
2018-12-13 17:21:58 +0800

    feat:Body

---
+++ components/Body/style/index.tsx
@@ -0,0 +1,11 @@
+/**
+ * Created by suki on 2018/12/13.
+ */
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f8f8f8'
+    }
+})
