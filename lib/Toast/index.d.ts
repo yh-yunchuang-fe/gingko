38bb5e0f274ab5c101cbca52c6dcead02e8fd3cb
Joey <zhangyi5628@126.com>
2019-02-15 15:03:28 +0800

    fix(toast):toast.loading返回类型为number

--- lib/Toast/index.d.ts
+++ lib/Toast/index.d.ts
@@ -6,11 +6,11 @@ import { IBaseToastOptions, IShowToastOptions } from './propsType';
 declare const _default: {
     LONG: number;
     SHORT: number;
-    success(content: string | React.ReactElement<any>, options?: IBaseToastOptions): null;
-    fail(content: string | React.ReactElement<any>, options?: IBaseToastOptions): null;
-    warn(content: string | React.ReactElement<any>, options?: IBaseToastOptions): null;
-    loading(content: string | React.ReactElement<any>, options?: IBaseToastOptions): null;
+    success(content: string | React.ReactElement<any>, options?: IBaseToastOptions): number;
+    fail(content: string | React.ReactElement<any>, options?: IBaseToastOptions): number;
+    warn(content: string | React.ReactElement<any>, options?: IBaseToastOptions): number;
+    loading(content: string | React.ReactElement<any>, options?: IBaseToastOptions): number;
     hide(id?: number | undefined): void;
-    show: (content: string | React.ReactElement<any>, options?: IShowToastOptions) => null;
+    show: (content: string | React.ReactElement<any>, options?: IShowToastOptions) => number;
 };
 export default _default;
