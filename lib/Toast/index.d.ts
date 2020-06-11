a135ec023c24eb5964becca9daeca1a95bdef482
赵俊 <335307524@qq.com>
2020-06-11 16:04:00 +0800

    修复Module组件

--- lib/Toast/index.d.ts
+++ lib/Toast/index.d.ts
@@ -6,11 +6,11 @@ import { IBaseToastOptions, IShowToastOptions } from './propsType';
 declare const _default: {
     LONG: number;
     SHORT: number;
-    success(content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IBaseToastOptions): any[];
-    fail(content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IBaseToastOptions): any[];
-    warn(content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IBaseToastOptions): any[];
-    loading(content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IBaseToastOptions): any[];
+    success(content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IBaseToastOptions): any;
+    fail(content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IBaseToastOptions): any;
+    warn(content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IBaseToastOptions): any;
+    loading(content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IBaseToastOptions): any;
     hide(): void;
-    show: (content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IShowToastOptions) => any[];
+    show: (content: string | React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)>) | (new (props: any) => React.Component<any, any, any>)>, options?: IShowToastOptions) => any;
 };
 export default _default;
